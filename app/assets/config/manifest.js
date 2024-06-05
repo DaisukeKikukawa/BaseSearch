0fb687a1757742d65500000798802a5f6f098c3d
daisuke-kikukawa <daisuke.kikukawa.16@gmail.com>
2024-06-05 22:23:59 +0900

    スタイル追加

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,8 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+//= link_directory ../stylesheets/admin .css
+//= link_directory ../stylesheets/common .css
+//= link_directory ../stylesheets/front .css
+//= link_directory ../stylesheets/front/common .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
